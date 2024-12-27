c588f267689c003b549f47c5904719495315a45a
baljeetgunghas2 <jaatmrharyanvi@gmail.com>
2024-12-28 00:10:37 +0530

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+
+
+export default function Home() {
+  return (
+    <>
+      <h1 className="text-3xl font-semibold text-center">Hello, Tailwind!</h1>
+      <p className="text-center">
+        This is a Tailwind CSS example in a Next.js app.
+      </p>
+    </>
+  );
+}
